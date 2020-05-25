7c0e9720770259a5ce15a9e304cd7d30ab4e0c68
Sadi Burak <sadiburak@Sadi-MacBook-Pro.local>
2020-05-25 19:54:34 +0300

    Yapay zeka 3

---
+++ dr3_dizgiler_string_boolean_op.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname dr3_dizgiler_string_boolean_op) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
+
+;https://htdp.org/2020-5-6/Book/part_one.html
+
+;Language->choose language advanced student
+
+(define s1 "merhaba")
+
+(string-length s1)
+
+(string-ith s1 3)
---
+++ dr3_dizgiler_string_boolean_op.rkt~
@@ -0,0 +1,17 @@
+#lang racket
+
+;https://htdp.org/2020-5-6/Book/part_one.html
+(+ 2 4)
+
+(* (+ 2 4) (+ 4 8))
+
+(+ 1 (+ 1 (+ 1 1) 2) 3 4 5)
+
+(sin 0)
+
+(define x 3)
+(define y 4)
+
+(sqrt(+ (* x x) (* y y)))
+
+(string-append "what a" "lovely" "day")
