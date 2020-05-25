f987bd94d03b1e26cb1e20d28de005d04389eb93
Sadi Burak <sadiburak@Sadi-MacBook-Pro.local>
2020-05-25 20:38:15 +0300

    Python 5

---
+++ dr4_kosullar_if_ve_cond_komutlar.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname dr4_kosullar_if_ve_cond_komutlar) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(require 2htdp/image)
+
+;https://htdp.org/2020-5-6/Book/part_one.html
+
+;Language->choose language advanced student
+
+
+
