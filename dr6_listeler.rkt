d90ba6ce5c7be58a07600427bdbca0b4abd007ca
Sadi Burak <sadiburak@Sadi-MacBook-Pro.local>
2020-05-26 12:40:12 +0300

    6 Listeler

---
+++ dr6_listeler.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname dr6_listeler) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+
+
+;https://htdp.org/2020-5-6/Book/part_one.html
+
+;Language->choose language advanced student önemli
+
+
+
+
+
